e062f5d7886f4fa4707a928472dcdd7b39ed83e6
rfcarnemolla <rfcarnemolla@wpi.edu>
2020-04-05 14:52:22 -0400

    created example graph

--- src/main/java/edu/wpi/teamF/pathfinding/Graph.java
+++ src/main/java/edu/wpi/teamF/pathfinding/Graph.java
@@ -11,17 +11,64 @@ public class Graph {
 
     public Graph(Set<GraphNode> nodes) {
         this.nodes = nodes;
+
+        //creation of example graph
+        GraphNode nodeA = new GraphNode(10, 6, "A");
+        GraphNode nodeB = new GraphNode(9, 2, "B");
+        GraphNode nodeC = new GraphNode(5, 7, "C");
+        GraphNode nodeD = new GraphNode(9, 9, "D");
+        GraphNode nodeE = new GraphNode(1, 2, "E");
+        GraphNode nodeF = new GraphNode(10, 1, "F");
+        GraphNode nodeG = new GraphNode(7, 4, "G");
+        GraphNode nodeH = new GraphNode(3, 1, "H");
+        GraphNode nodeI = new GraphNode(5, 4, "I");
+        GraphNode nodeJ = new GraphNode(9, 1, "J");
+
+        nodeA.addNeighbor(nodeD);
+        nodeA.addNeighbor(nodeG);
+        nodeA.addNeighbor(nodeB);
+        nodeA.addNeighbor(nodeF);
+        nodeB.addNeighbor(nodeJ);
+        nodeB.addNeighbor(nodeG);
+        nodeB.addNeighbor(nodeF);
+        nodeB.addNeighbor(nodeA);
+        nodeC.addNeighbor(nodeD);
+        nodeD.addNeighbor(nodeA);
+        nodeB.addNeighbor(nodeC);
+        nodeE.addNeighbor(nodeH);
+        nodeE.addNeighbor(nodeI);
+        nodeF.addNeighbor(nodeJ);
+        nodeF.addNeighbor(nodeB);
+        nodeF.addNeighbor(nodeA);
+        nodeG.addNeighbor(nodeI);
+        nodeG.addNeighbor(nodeA);
+        nodeG.addNeighbor(nodeB);
+        nodeH.addNeighbor(nodeE);
+        nodeH.addNeighbor(nodeI);
+        nodeI.addNeighbor(nodeE);
+        nodeI.addNeighbor(nodeH);
+        nodeI.addNeighbor(nodeG);
+        nodeI.addNeighbor(nodeJ);
+        nodeJ.addNeighbor(nodeB);
+        nodeJ.addNeighbor(nodeF);
+        nodeJ.addNeighbor(nodeI);
+
+        nodes.add(nodeA);
+        nodes.add(nodeB);
+        nodes.add(nodeC);
+        nodes.add(nodeD);
+        nodes.add(nodeE);
+        nodes.add(nodeF);
+        nodes.add(nodeG);
+        nodes.add(nodeH);
+        nodes.add(nodeI);
+        nodes.add(nodeJ);
+
     }
 
     public Set<GraphNode> getNodes(){
         return nodes;
     }
-
-    //not sure if this should be in this class
-    GraphNode nodeA = new GraphNode(10, 6, "A");
-    GraphNode nodeB = new GraphNode(9, 2, "B");
-    Set<GraphNode> newSet = new HashSet<GraphNode>();
-    //newSet.add(nodeA); //doesn't like add for some reason
 
     public GraphNode getNode(String find){
         for(GraphNode i: this.nodes){
