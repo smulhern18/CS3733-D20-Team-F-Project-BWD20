cea9e22b69f1ddba00cc9a0be963c9a32454dc3d
denver blake <denver.blake26@gmail.com>
2020-04-04 21:08:38 -0400

    changed routeNode to comparable and add setters

--- src/main/java/edu/wpi/teamF/pathfinding/RouteNode.java
+++ src/main/java/edu/wpi/teamF/pathfinding/RouteNode.java
@@ -1,11 +1,15 @@
 package edu.wpi.teamF.pathfinding;
 
-public class RouteNode {
+public class RouteNode implements Comparable<RouteNode> {
 
-    private final GraphNode current;
-    private final GraphNode previous;
-    private final double routeScore;
-    private final double estimatedScore;
+    private  GraphNode current;
+    private  GraphNode previous;
+    private  double routeScore;
+    private  double estimatedScore;
+
+    public RouteNode (GraphNode current) {
+        this(current, null, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
     public RouteNode(GraphNode current, GraphNode previous, double routeScore, double estimatedScore) {
 
         this.current = current;
@@ -19,15 +23,42 @@ public class RouteNode {
         return current;
     }
 
+    public  void setCurrent(GraphNode current) {
+        this.current = current;
+    }
+
     public GraphNode getPrevious() {
         return previous;
+    }
+
+    public void setPrevious(GraphNode previous) {
+        this.previous = previous;
     }
 
     public double getRouteScore() {
         return routeScore;
     }
 
+    public void setRouteScore(double routeScore) {
+        this.routeScore = routeScore;
+    }
+
     public double getEstimatedScore() {
         return estimatedScore;
+    }
+
+    public void setEstimatedScore(double estimatedScore) {
+        this.estimatedScore = estimatedScore;
+    }
+
+    @Override
+    public int compareTo(RouteNode other) {
+        if (this.estimatedScore > other.estimatedScore) {
+            return 1;
+        } else if (this.estimatedScore < other.estimatedScore) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
