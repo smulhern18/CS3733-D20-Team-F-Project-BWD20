c42859db851e5a9a8a556de6e29a104897421da2
Reilly Norum <renorum@wpi.edu>
2020-04-11 14:31:35 -0400

    first design of display data screen

--- src/main/java/edu/wpi/teamF/controllers/DisplayDataController.java
+++ src/main/java/edu/wpi/teamF/controllers/DisplayDataController.java
@@ -1,4 +1,21 @@
 package edu.wpi.teamF.controllers;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class DisplayDataController extends SceneController {
+
+  public void switchToMain(ActionEvent actionEvent) throws IOException {
+    Parent mainMenu = FXMLLoader.load(getClass().getResource("/edu/wpi/teamF/views/MainMenu.fxml"));
+    Scene mainMenuScene = new Scene(mainMenu);
+    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+    appStage.setScene(mainMenuScene);
+    appStage.show();
+  }
 }
