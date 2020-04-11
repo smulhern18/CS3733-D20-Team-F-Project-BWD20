71ab86e85b4c97344dd75a81fb886a8e6dc37c66
Tyler <twjones@wpi.edu>
2020-04-11 14:39:49 -0400

    updates main menu and scene switcher

--- src/main/java/edu/wpi/teamF/controllers/MainMenuController.java
+++ src/main/java/edu/wpi/teamF/controllers/MainMenuController.java
@@ -1,4 +1,25 @@
 package edu.wpi.teamF.controllers;
 
-public class MainMenuController {
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class MainMenuController extends SceneController {
+
+  SceneController sceneController = new SceneController();
+
+  public void switchToDisplayData(ActionEvent actionEvent) throws IOException {
+    sceneController.switchScene(actionEvent, "DisplayData");
+  }
+
+  public void switchToDownloadData(ActionEvent actionEvent) throws IOException {
+    sceneController.switchScene(actionEvent, "DownloadData");
+  }
+
+  public void switchToModifyValues(ActionEvent actionEvent) throws IOException {
+    sceneController.switchScene(actionEvent, "ModifyValues");
+  }
+
+  public void switchToPathfinder(ActionEvent actionEvent) throws IOException {
+    sceneController.switchScene(actionEvent, "Pathfinder");
+  }
 }
