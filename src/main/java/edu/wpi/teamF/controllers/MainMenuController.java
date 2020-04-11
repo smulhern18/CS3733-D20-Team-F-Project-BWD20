986cb90942e7dfc83fa8df33a9567609d0914ff2
Roman Wicky <rwickyvd@gmail.com>
2020-04-11 15:20:13 -0400

    fixed scene switcher

--- src/main/java/edu/wpi/teamF/controllers/MainMenuController.java
+++ src/main/java/edu/wpi/teamF/controllers/MainMenuController.java
@@ -8,18 +8,18 @@ public class MainMenuController extends SceneController {
   SceneController sceneController = new SceneController();
 
   public void switchToDisplayData(ActionEvent actionEvent) throws IOException {
-    sceneController.switchScene(actionEvent, "DisplayData");
+    sceneController.switchScene("DisplayData");
   }
 
   public void switchToDownloadData(ActionEvent actionEvent) throws IOException {
-    sceneController.switchScene(actionEvent, "DownloadData");
+    sceneController.switchScene("DownloadData");
   }
 
   public void switchToModifyValues(ActionEvent actionEvent) throws IOException {
-    sceneController.switchScene(actionEvent, "ModifyValues");
+    sceneController.switchScene("ModifyValues");
   }
 
   public void switchToPathfinder(ActionEvent actionEvent) throws IOException {
-    sceneController.switchScene(actionEvent, "Pathfinder");
+    sceneController.switchScene("Pathfinder");
   }
 }
--- src/main/java/edu/wpi/teamF/controllers/SceneController.java
+++ src/main/java/edu/wpi/teamF/controllers/SceneController.java
@@ -1,20 +1,18 @@
 package edu.wpi.teamF.controllers;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class SceneController {
 
-  public void switchScene(ActionEvent actionEvent, String aScene) throws IOException {
-    Parent newFile = FXMLLoader.load(getClass().getResource("/edu/wpi/teamF/views/" + aScene + ".fxml"));
+  public void switchScene(String aScene) throws IOException {
+    Parent newFile =
+        FXMLLoader.load(getClass().getResource("/edu/wpi/teamF/views/" + aScene + ".fxml"));
     Scene newScene = new Scene(newFile);
-    Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-
+    Stage appStage = new Stage();
     appStage.setScene(newScene);
     appStage.show();
   }
