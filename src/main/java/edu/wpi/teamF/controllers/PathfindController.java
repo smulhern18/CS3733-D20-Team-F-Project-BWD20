a41efc11eb9ffbca6b77ce9814b1da8f6d279d02
rfcarnemolla <rfcarnemolla@wpi.edu>
2020-04-11 12:15:25 -0400

    PathfindController attribute

--- src/main/java/edu/wpi/teamF/controllers/PathfindController.java
+++ src/main/java/edu/wpi/teamF/controllers/PathfindController.java
@@ -1,4 +1,22 @@
 package edu.wpi.teamF.controllers;
 
+import edu.wpi.teamF.factories.NodeFactory;
+
 public class PathfindController extends SceneController {
+
+    private NodeFactory nodeFactory;
+
+    public PathfindController(NodeFactory nodeFactory){
+        this.nodeFactory = nodeFactory;
+    }
+
+    public NodeFactory getNodeFactory() {
+        return nodeFactory;
+    }
+
+    public void setNodeFactory(NodeFactory nodeFactory) {
+        this.nodeFactory = nodeFactory;
+    }
+
+
 }
