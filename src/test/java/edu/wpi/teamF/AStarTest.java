66770c3f98ddf7f8d30a707649f8bd57aba03603
kaboenisch <kaboenisch@wpi.edu>
2020-04-11 15:07:31 -0400

    Completed ElevScor2 test

--- src/test/java/edu/wpi/teamF/AStarTest.java
+++ src/test/java/edu/wpi/teamF/AStarTest.java
@@ -2,7 +2,7 @@ package edu.wpi.teamF;
 
 import edu.wpi.teamF.modelClasses.ElevatorScorer2;
 import edu.wpi.teamF.modelClasses.Node;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,8 +15,10 @@ public class AStarTest {
         Node elev2 = new Node(10,10,"elev2", Node.NodeType.ELEV, 1);
         Node elev3 = new Node(8,3,"elev3", Node.NodeType.ELEV, 1);
         List<Node> elevators = Arrays.asList(elev1, elev2, elev3);
+        Node start = new Node(3,2,"start", Node.NodeType.HALL, 1);
+        Node end = new Node(10,2,"end", Node.NodeType.HALL, 1);
         ElevatorScorer2 tester = new ElevatorScorer2(elevators);
 
-        assertEquals();
+        Assertions.assertEquals(elev1, tester.elevatorScorer(start,end), "Elevator 1 is closest");
     }
 }
