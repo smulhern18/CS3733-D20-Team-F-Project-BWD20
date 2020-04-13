bad7b593ec09ed8ea46e2e30a76bc63910529d57
Tyler <twjones@wpi.edu>
2020-04-13 18:42:55 -0400

    Modify Data scene

--- src/main/java/edu/wpi/teamF/controllers/ModifyDataController.java
+++ src/main/java/edu/wpi/teamF/controllers/ModifyDataController.java
@@ -1,7 +1,11 @@
 package edu.wpi.teamF.controllers;
 
+import java.awt.*;
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ModifyDataController extends SceneController {
   SceneController sceneController = new SceneController();
@@ -21,4 +25,24 @@ public class ModifyDataController extends SceneController {
   public void displaySwitch(ActionEvent actionEvent) throws IOException {
     sceneController.switchScene("DisplayData");
   }
+
+  @FXML private TextField nodeText;
+  @FXML private Button addButton;
+  @FXML private Button deleteButton;
+  @FXML private Button editButton;
+
+  @FXML
+  private void validateButton() {
+    if (!nodeText.getText().isEmpty()) {
+      addButton.setDisable(false);
+      deleteButton.setDisable(false);
+      editButton.setDisable(false);
+    }
+  }
+
+  public void addNode(ActionEvent actionEvent) {}
+
+  public void deleteNode(ActionEvent actionEvent) {}
+
+  public void editNode(ActionEvent actionEvent) {}
 }
