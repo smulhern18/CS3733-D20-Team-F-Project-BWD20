82685bf10e71299b1b3a1ce01c5b63a7b0eeec61
sjmulhern <sullivanmulhern@gmail.com>
2020-04-17 16:07:13 -0400

    Fixed the gitignore and edited the database manager

--- src/main/java/edu/wpi/teamF/DatabaseManipulators/DatabaseManager.java
+++ src/main/java/edu/wpi/teamF/DatabaseManipulators/DatabaseManager.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamF.Factories;
+package edu.wpi.teamF.DatabaseManipulators;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,11 +10,11 @@ public class DatabaseManager {
   /** Table names */
   static final String NODES_TABLE_NAME = "nodesTable";
   static final String EDGES_TABLE_NAME = "edgesTable";
-  static final String SERVICEREQUEST_TABLE_NAME = "serviceRequestTable";
-  static final String USER_TABLE_NAME = "userTable";
+  static final String SERVICEREQUEST_TABLE_NAME = "serviceRequestsTable";
+  static final String USER_TABLE_NAME = "usersTable";
   /** Column Names */
   //node
-  static final String NODE_NAME_KEY = "nodeName";
+  static final String NODE_NAME_KEY = "nodeId";
   static final String X_COORDINATE_KEY = "xCoord";
   static final String Y_COORDINATE_KEY = "yCoord";
   static final String BUILDING_KEY = "building";
@@ -23,19 +23,21 @@ public class DatabaseManager {
   static final String TYPE_KEY = "type";
   static final String FLOOR_KEY = "floor";
   //edge
-  static final String EDGEID_KEY = "edgeID";
+  static final String EDGEID_KEY = "edgeId";
   static final String NODE_1_KEY = "node1";
   static final String NODE_A_KEY = "nodeA";
+
   //service Request
-  static final String SERVICEID_KEY = "serviceD";
-  static final String NODEID_KEY = "nodeID";
-  static final String TIME_CREATED_KEY = "TimeCreated";
+  static final String SERVICEID_KEY = "serviceId";
+  static final String NODEID_KEY = "nodeId";
+  static final String DESCRIPTION_KEY = "description";
+  static final String TIME_CREATED_KEY = "timeCreated";
+  static final String PRIORITY_KEY = "priority";
 
   //User
   static final String USER_NAME_KEY = "userName";
   static final String PASSWORD_KEY = "password";
   static final String FIRST_NAME_KEY = "firstName";
-  static final String MIDDLE_NAME_KEY = "middleName";
   static final String LAST_NAME_KEY = "lastName";
   static final String ADDRESS_KEY = "address";
   static final String EMAIL_KEY = "email";
@@ -46,80 +48,82 @@ public class DatabaseManager {
 
   public void createTables() throws SQLException {
     String nodeTableCreationStatement =
-        "CREATE TABLE "
-            + NODES_TABLE_NAME
-            + " ( "
-            + NODE_NAME_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + X_COORDINATE_KEY
-            + " SMALLINT NOT NULL, "
-            + Y_COORDINATE_KEY
-            + " SMALLINT NOT NULL, "
-            + BUILDING_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + LONG_NAME_KEY
-            + " VARCHAR(64) NOT NULL, "
-            + SHORT_NAME_KEY
-            + " VARCHAR(16) NOT NULL, "
-            + TYPE_KEY
-            + " VARCHAR(4) NOT NULL, "
-            + FLOOR_KEY
-            + " SMALLINT NOT NULL, "
-            + "PRIMARY KEY ("
-            + NODE_NAME_KEY
-            + "))";
+            "CREATE TABLE "
+                    + NODES_TABLE_NAME
+                    + " ( "
+                    + NODE_NAME_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + X_COORDINATE_KEY
+                    + " SMALLINT NOT NULL, "
+                    + Y_COORDINATE_KEY
+                    + " SMALLINT NOT NULL, "
+                    + BUILDING_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + LONG_NAME_KEY
+                    + " VARCHAR(64) NOT NULL, "
+                    + SHORT_NAME_KEY
+                    + " VARCHAR(16) NOT NULL, "
+                    + TYPE_KEY
+                    + " VARCHAR(4) NOT NULL, "
+                    + FLOOR_KEY
+                    + " SMALLINT NOT NULL, "
+                    + "PRIMARY KEY ("
+                    + NODE_NAME_KEY
+                    + "))";
 
     String edgeTableCreationStatement =
-        "CREATE TABLE "
-            + EDGES_TABLE_NAME
-            + " ( "
-            + EDGEID_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + NODE_1_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + NODE_A_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + "PRIMARY KEY ("
-            + EDGEID_KEY
-            + "))";
+            "CREATE TABLE "
+                    + EDGES_TABLE_NAME
+                    + " ( "
+                    + EDGEID_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + NODE_1_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + NODE_A_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + "PRIMARY KEY ("
+                    + EDGEID_KEY
+                    + "))";
 
     String serviceTableCreationStatement =
-        "CREATE TABLE "
-            + SERVICEREQUEST_TABLE_NAME
-            + " ( "
-            + SERVICEID_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + NODEID_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + TIME_CREATED_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + "PRIMARY KEY ("
-            + SERVICEID_KEY
-            + "))";
+            "CREATE TABLE "
+                    + SERVICEREQUEST_TABLE_NAME
+                    + " ( "
+                    + SERVICEID_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + NODEID_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + TIME_CREATED_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + DESCRIPTION_KEY
+                    + " VARCHAR(128) NOT NULL, "
+                    + PRIORITY_KEY
+                    + " INTEGER NOT NULL, "
+                    + "PRIMARY KEY ("
+                    + SERVICEID_KEY
+                    + "))";
 
     String userTableCreationStatement =
-        "CREATE TABLE "
-            + USER_TABLE_NAME
-            + " ( "
-            + USER_NAME_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + PASSWORD_KEY
-            + " SMALLINT NOT NULL, "
-            + FIRST_NAME_KEY
-            + " SMALLINT NOT NULL, "
-            + MIDDLE_NAME_KEY
-            + " VARCHAR(32) NOT NULL, "
-            + LAST_NAME_KEY
-            + " VARCHAR(64) NOT NULL, "
-            + ADDRESS_KEY
-            + " VARCHAR(16) NOT NULL, "
-            + EMAIL_KEY
-            + " VARCHAR(4) NOT NULL, "
-            + USER_TYPE_KEY
-            + " SMALLINT NOT NULL, "
-            + "PRIMARY KEY ("
-            + USER_NAME_KEY
-            + "))";
+            "CREATE TABLE "
+                    + USER_TABLE_NAME
+                    + " ( "
+                    + USER_NAME_KEY
+                    + " VARCHAR(32) NOT NULL, "
+                    + PASSWORD_KEY
+                    + " INTEGER NOT NULL, "
+                    + FIRST_NAME_KEY
+                    + " INTEGER NOT NULL, "
+                    + LAST_NAME_KEY
+                    + " VARCHAR(64) NOT NULL, "
+                    + ADDRESS_KEY
+                    + " VARCHAR(16) NOT NULL, "
+                    + EMAIL_KEY
+                    + " VARCHAR(4) NOT NULL, "
+                    + USER_TYPE_KEY
+                    + " SMALLINT NOT NULL, "
+                    + "PRIMARY KEY ("
+                    + USER_NAME_KEY
+                    + "))";
 
     PreparedStatement preparedStatement = connection.prepareStatement(nodeTableCreationStatement);
     preparedStatement.execute();
