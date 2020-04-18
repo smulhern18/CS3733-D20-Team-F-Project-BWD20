f2ca5c32d3ff1af8a1e410745d0f3f842f5ad7dd
Tyler <twjones@wpi.edu>
2020-04-18 11:45:28 -0400

    updated login controller

--- src/main/java/edu/wpi/teamF/Controllers/LoginController.java
+++ src/main/java/edu/wpi/teamF/Controllers/LoginController.java
@@ -1,3 +1,42 @@
 package edu.wpi.teamF.Controllers;
 
-public class LoginController {}
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class LoginController {
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private JFXTextField usernameInput;
+
+    @FXML
+    private JFXPasswordField passwordInput;
+
+    @FXML
+    private JFXButton RegisterButton;
+
+    private AccountFactory accountFactory = AccountFactory.getFactory();
+
+    @FXML
+    void AttemptLogin(ActionEvent event) {
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
+
+    }
+
+    @FXML
+    void switchToMainMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToRegister(ActionEvent event) {
+
+    }
+}
