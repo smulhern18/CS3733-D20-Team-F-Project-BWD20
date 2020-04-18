0b7556caa16f71a95964d18aef8157ca0b3070ff
rfcarnemolla <rfcarnemolla@wpi.edu>
2020-04-17 22:07:51 -0400

    path, routeNode, EuclideanScorer

--- src/main/java/edu/wpi/teamF/ModelClasses/RouteNode.java
+++ src/main/java/edu/wpi/teamF/ModelClasses/RouteNode.java
@@ -10,9 +10,59 @@ public class RouteNode implements Comparable<RouteNode> {
   private double routeScore;
   private double estimatedScore;
 
-  @Override
-  public int compareTo(RouteNode o) {
-    return 0;
+  public RouteNode(Node node) {
+    this(node, null, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
   }
 
+  public RouteNode(Node node, RouteNode previous, double routeScore, double estimatedScore) {
+
+    this.node = node;
+    this.previous = previous;
+
+    this.routeScore = routeScore;
+    this.estimatedScore = estimatedScore;
+  }
+
+  public Node getNode() {
+    return node;
+  }
+
+  public void setNode(Node node) {
+    this.node = node;
+  }
+
+  public RouteNode getPrevious() {
+    return previous;
+  }
+
+  public void setPrevious(RouteNode previous) {
+    this.previous = previous;
+  }
+
+  public double getRouteScore() {
+    return routeScore;
+  }
+
+  public void setRouteScore(double routeScore) {
+    this.routeScore = routeScore;
+  }
+
+  public double getEstimatedScore() {
+    return estimatedScore;
+  }
+
+  public void setEstimatedScore(double estimatedScore) {
+    this.estimatedScore = estimatedScore;
+  }
+
+  @Override
+  public int compareTo(RouteNode other) {
+    if (this.estimatedScore > other.estimatedScore) {
+      return 1;
+    } else if (this.estimatedScore < other.estimatedScore) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
 }
--- src/main/java/edu/wpi/teamF/ModelClasses/Scorer/EuclideanScorer.java
+++ src/main/java/edu/wpi/teamF/ModelClasses/Scorer/EuclideanScorer.java
@@ -3,9 +3,9 @@ package edu.wpi.teamF.ModelClasses.Scorer;
 import edu.wpi.teamF.ModelClasses.Node;
 
 public class EuclideanScorer implements Scorer {
-  @Override
-  public double computeCost(Node start, Node end) {
-    return 0;
+  public double computeCost(Node from, Node to) {
+    return Math.sqrt(
+            Math.pow(from.getXCoord() - to.getXCoord(), 2)
+                    + Math.pow(from.getYCoord() - to.getYCoord(), 2));
   }
-
 }
