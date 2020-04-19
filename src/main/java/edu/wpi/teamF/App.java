3620d62ee955dbc71e1685d45cd125c4ce8da2d4
Reilly Norum <renorum@wpi.edu>
2020-04-18 21:15:50 -0400

    merging

--- src/main/java/edu/wpi/teamF/App.java
+++ src/main/java/edu/wpi/teamF/App.java
@@ -27,6 +27,7 @@ public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
+
     try {
       Parent root = FXMLLoader.load(getClass().getResource("views/MainMenu.fxml"));
       Scene scene = new Scene(root);
--- src/main/java/edu/wpi/teamF/Controllers/MainMenuController.java
+++ src/main/java/edu/wpi/teamF/Controllers/MainMenuController.java
@@ -1,3 +1,59 @@
 package edu.wpi.teamF.Controllers;
 
-public class MainMenuController {}
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class MainMenuController {
+
+  @FXML private AnchorPane root;
+
+  private void loadSplashScreen() {
+    try {
+      // load splash screen view
+      StackPane pane = FXMLLoader.load(getClass().getResource(("Splash.fxml")));
+      // add to root container
+      root.getChildren().setAll(pane);
+
+      // load splash screen with fade in
+      FadeTransition fadeIn = new FadeTransition(Duration.seconds(3), pane);
+      fadeIn.setFromValue(0);
+      fadeIn.setToValue(1);
+      fadeIn.setCycleCount(1);
+
+      // fade out
+      FadeTransition fadeOut = new FadeTransition(Duration.seconds(3), pane);
+      fadeOut.setFromValue(1);
+      fadeOut.setToValue(0);
+      fadeOut.setCycleCount(1);
+
+      fadeIn.play();
+
+      // after fade in, fade out
+      fadeIn.setOnFinished(
+          (e) -> {
+            fadeOut.play();
+          });
+
+      // after fade out, show main menu
+      fadeOut.setOnFinished(
+          (e) -> {
+            try {
+              AnchorPane main = FXMLLoader.load(getClass().getResource(("MainMenu.fxml")));
+              root.getChildren().setAll(main);
+            } catch (IOException ex) {
+              Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+          });
+
+    } catch (IOException ex) {
+      Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+}
