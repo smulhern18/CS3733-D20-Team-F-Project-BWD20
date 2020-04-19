5f43610d10d9070dbc1a2e3aad3723d0a8f07346
Tyler <twjones@wpi.edu>
2020-04-18 22:17:18 -0400

    login and registration page complete

--- src/main/java/edu/wpi/teamF/Controllers/LoginController.java
+++ src/main/java/edu/wpi/teamF/Controllers/LoginController.java
@@ -46,6 +46,7 @@ public class LoginController {
     Account account = accountFactory.getAccountByUsername(username);
     if (account != null && account.getPassword().equals(password)) {
       System.out.println("The account is valid");
+      // code that logs the user into the application
     } else {
       incorrectLabel.setVisible(true);
       usernameInput.setUnFocusColor(Color.RED);
