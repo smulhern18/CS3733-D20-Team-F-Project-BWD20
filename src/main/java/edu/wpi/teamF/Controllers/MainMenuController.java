86bc9e0c9f6cc1bed4b1b0374a4dfb6a40c6442d
myHeMarina <47702043+myHeMarina@users.noreply.github.com>
2020-04-20 05:48:30 +0800

    a

--- src/main/java/edu/wpi/teamF/Controllers/MainMenuController.java
+++ src/main/java/edu/wpi/teamF/Controllers/MainMenuController.java
@@ -1,8 +1,5 @@
 package edu.wpi.teamF.Controllers;
 
-import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
 public class MainMenuController {
