ab32cc09124f43ff28d07d6073e9940db499eee9
Tyler <twjones@wpi.edu>
2020-04-19 20:31:41 -0400

    modify map data page (nodes) complete

--- src/main/java/edu/wpi/teamF/Controllers/DataMapViewController.java
+++ src/main/java/edu/wpi/teamF/Controllers/DataMapViewController.java
@@ -1,57 +1,51 @@
 package edu.wpi.teamF.Controllers;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.teamF.DatabaseManipulators.EdgeFactory;
 import edu.wpi.teamF.DatabaseManipulators.NodeFactory;
 import edu.wpi.teamF.ModelClasses.Node;
+import edu.wpi.teamF.ModelClasses.ValidationException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
 public class DataMapViewController implements Initializable {
-  @FXML
-  private AnchorPane mapPane;
+  @FXML private AnchorPane mapPane;
 
-  @FXML
-  private StackPane modifyPane;
+  @FXML private StackPane modifyPane;
 
-  @FXML
-  private JFXButton deleteButton;
+  @FXML private JFXButton addButton;
 
-  @FXML
-  private JFXButton modifyButton;
+  @FXML private JFXButton deleteButton;
 
-  @FXML
-  private JFXTextField nodeIDInput;
+  @FXML private JFXButton modifyButton;
 
-  @FXML
-  private JFXTextField yCoorInput;
+  @FXML private JFXTextField nodeIDInput;
 
-  @FXML
-  private JFXTextField xCoorInput;
+  @FXML private JFXTextField yCoorInput;
 
-  @FXML
-  private JFXTextField typeInput;
+  @FXML private JFXTextField xCoorInput;
 
-  @FXML
-  private JFXTextField shortNameInput;
+  @FXML private JFXTextField typeInput;
 
-  @FXML
-  private JFXTextField longNameInput;
+  @FXML private JFXTextField shortNameInput;
 
-  @FXML
-  private JFXTextField buildingInput;
+  @FXML private JFXTextField longNameInput;
 
-  @FXML
-  private JFXTextField floorInput;
+  @FXML private JFXTextField buildingInput;
 
+  @FXML private JFXTextField floorInput;
+
+  @FXML private JFXButton addButtonWindow;
+
+  JFXButton nodeButton = null;
+
+  Node node = null;
 
   private static final int MAP_HEIGHT = 1485;
   private static final int MAP_WIDTH = 2475;
@@ -71,7 +65,7 @@ public class DataMapViewController implements Initializable {
     double heightRatio = mapPane.getHeight() / MAP_HEIGHT;
     double widthRatio = mapPane.getWidth() / MAP_WIDTH;
 
-    Button button = new Button();
+    JFXButton button = new JFXButton();
     button.setMinSize(12, 12);
     button.setMaxSize(12, 12);
     button.setPrefSize(12, 12);
@@ -83,7 +77,11 @@ public class DataMapViewController implements Initializable {
     button.setLayoutY(yPos);
     button.setOnAction(
         action -> {
+          nodeButton = button;
+          this.node = node;
           modifyPane.setVisible(true);
+          modifyButton.setDisable(true);
+          displayData();
         });
     mapPane.getChildren().add(button);
   }
@@ -92,5 +90,133 @@ public class DataMapViewController implements Initializable {
   public void initialize(URL location, ResourceBundle resources) {
     drawNode(null);
     System.out.println("Test");
+  }
+
+  private enum ModifyType {
+    DELETE,
+    ADD,
+    EDIT
+  };
+
+  private ModifyType modifyType;
+
+  @FXML
+  private void cancel() {
+    reset();
+    modifyPane.setVisible(false);
+  }
+
+  @FXML
+  private void deleteData() {
+    nodeFactory.delete(node.getId()); // in the database
+    mapPane.getChildren().remove(nodeButton); // on the map
+    reset();
+  }
+
+  @FXML
+  private void addWindow() throws ValidationException {
+    modifyPane.setVisible(true);
+    modifyButton.setVisible(false);
+    deleteButton.setVisible(false);
+    addButtonWindow.setVisible(true);
+    addButtonWindow.setDisable(true);
+  }
+
+  @FXML
+  private void addData() throws ValidationException {
+    String ID = nodeIDInput.getText();
+    short xCoordinate = Short.parseShort(xCoorInput.getText());
+    short yCoordinate = Short.parseShort(yCoorInput.getText());
+    String building = buildingInput.getText();
+    String longName = longNameInput.getText();
+    String shortName = shortNameInput.getText();
+    Node.NodeType nodeType = Node.NodeType.getEnum(typeInput.getText());
+    short floorNumber = Short.parseShort(floorInput.getText());
+
+    node.setId(ID);
+    node.setXCoord(xCoordinate);
+    node.setYCoord(yCoordinate);
+    node.setBuilding(building);
+    node.setLongName(longName);
+    node.setShortName(shortName);
+    node.setType(nodeType);
+    node.setFloor(floorNumber);
+
+    nodeFactory.create(node);
+    drawNode(node);
+    reset();
+    modifyPane.setVisible(false);
+  }
+
+  @FXML
+  private void modifyData() throws ValidationException {
+    String ID = nodeIDInput.getText();
+    short xCoordinate = Short.parseShort(xCoorInput.getText());
+    short yCoordinate = Short.parseShort(yCoorInput.getText());
+    String building = buildingInput.getText();
+    String longName = longNameInput.getText();
+    String shortName = shortNameInput.getText();
+    Node.NodeType nodeType = Node.NodeType.getEnum(typeInput.getText());
+    short floorNumber = Short.parseShort(floorInput.getText());
+
+    node.setId(ID);
+    node.setXCoord(xCoordinate);
+    node.setYCoord(yCoordinate);
+    node.setBuilding(building);
+    node.setLongName(longName);
+    node.setShortName(shortName);
+    node.setType(nodeType);
+    node.setFloor(floorNumber);
+
+    nodeFactory.update(node);
+    double heightRatio = mapPane.getHeight() / MAP_HEIGHT;
+    double widthRatio = mapPane.getWidth() / MAP_WIDTH;
+    nodeButton.setLayoutX(xCoordinate * widthRatio);
+    nodeButton.setLayoutY(yCoordinate * heightRatio);
+    reset();
+  }
+
+  @FXML
+  private void displayData() {
+    nodeIDInput.setText(node.getId());
+    yCoorInput.setText("" + node.getYCoord());
+    xCoorInput.setText("" + node.getXCoord());
+    buildingInput.setText(node.getBuilding());
+    longNameInput.setText(node.getLongName());
+    shortNameInput.setText(node.getShortName());
+    typeInput.setText("" + node.getType());
+    floorInput.setText("" + node.getFloor());
+  }
+
+  @FXML
+  public void validateText(KeyEvent keyEvent) {
+    if (!xCoorInput.getText().isEmpty()
+        && !yCoorInput.getText().isEmpty()
+        && !buildingInput.getText().isEmpty()
+        && !longNameInput.getText().isEmpty()
+        && !shortNameInput.getText().isEmpty()
+        && !typeInput.getText().isEmpty()
+        && !floorInput.getText().isEmpty()) {
+      modifyButton.setDisable(false);
+      addButtonWindow.setDisable(false);
+    } else {
+      modifyButton.setDisable(true);
+      addButtonWindow.setDisable(true);
+    }
+  }
+
+  @FXML
+  private void reset() {
+    xCoorInput.setText("");
+    yCoorInput.setText("");
+    buildingInput.setText("");
+    longNameInput.setText("");
+    shortNameInput.setText("");
+    typeInput.setText("");
+    floorInput.setText("");
+    nodeIDInput.setText("");
+    modifyButton.setVisible(true);
+    deleteButton.setVisible(true);
+    addButtonWindow.setVisible(false);
   }
 }
