a888c80eaf0864cd70af155a15b8bf4d4b41c150
Roman Wicky <rwickyvd@gmail.com>
2020-04-21 23:09:24 -0400

    final submission

--- src/main/java/edu/wpi/teamF/Main.java
+++ src/main/java/edu/wpi/teamF/Main.java
@@ -1,13 +1,13 @@
 package edu.wpi.teamF;
 
-import edu.wpi.teamF.DatabaseManipulators.AccountFactory;
-import edu.wpi.teamF.DatabaseManipulators.CSVManipulator;
-import edu.wpi.teamF.DatabaseManipulators.DatabaseManager;
+import edu.wpi.teamF.DatabaseManipulators.*;
 import edu.wpi.teamF.ModelClasses.Account.Admin;
 
 public class Main {
   private static CSVManipulator csvm = new CSVManipulator();
   private static DatabaseManager dbm = new DatabaseManager();
+  private static NodeFactory nodeFactory = NodeFactory.getFactory();
+  private static EdgeFactory edgeFactory = EdgeFactory.getFactory();
 
   public static void main(String[] args) throws Exception {
 
@@ -24,8 +24,10 @@ public class Main {
   public static void initDB() throws Exception {
     dbm.initialize();
     AccountFactory accounts = AccountFactory.getFactory();
-    csvm.readCSVFileNode(Main.class.getResourceAsStream("CSVFiles/MapFAllnodes.csv"));
-    csvm.readCSVFileEdge(Main.class.getResourceAsStream("CSVFiles/MapFAlledges.csv"));
+    if (nodeFactory.getAllNodes() == null && edgeFactory.getAllEdges() == null) {
+      csvm.readCSVFileNode(Main.class.getResourceAsStream("CSVFiles/MapFAllnodes.csv"));
+      csvm.readCSVFileEdge(Main.class.getResourceAsStream("CSVFiles/MapFAlledges.csv"));
+    }
     if (accounts.read("admin") == null) {
       accounts.create(new Admin("admin", "admin", "admin@gmail.com", "admin", "password"));
     }
