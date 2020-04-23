f562b88d666b9986514a477d0e2b59dd57ed6bfa
denver blake <denver.blake26@gmail.com>
2020-04-23 16:58:24 -0400

    working zoom in zoom test fxml

--- src/main/java/edu/wpi/teamF/Controllers/ZoomController.java
+++ src/main/java/edu/wpi/teamF/Controllers/ZoomController.java
@@ -1,47 +1,108 @@
 package edu.wpi.teamF.Controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.input.ScrollEvent;
-
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-
-
-import javax.swing.text.html.ImageView;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class ZoomController implements Initializable {
 
-    @FXML
-    private ScrollPane scrollPane;
+  private Group zoomNode;
 
-    @FXML
-    private ImageView imaeg;
+  private double scaleValue;
+  private double zoomIntensity = 0.02;
 
+  @FXML private ScrollPane scrollPane;
+  @FXML private StackPane mapPane;
 
-    public void zoom(ScrollEvent event) {
-        double zoomFactor = 1.5;
-        if (event.getDeltaY() <= 0) {
-            // zoom out
-            zoomFactor = 1 / zoomFactor;
-        }
-        scrollPane.getViewportBounds()
+  @FXML private ImageView mapImage;
+  @FXML private Label testLabel;
 
-    }
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    // zoomPane = new ZoomableScrollPane(scrollPane);
+    zoomNode = new Group(mapPane);
+    scrollPane.setContent(outerNode(zoomNode));
 
+    scrollPane.setPannable(true);
+    scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    scrollPane.setFitToHeight(true); // center
+    scrollPane.setFitToWidth(true); // center
+    scaleValue = 1;
+    updateScale();
+  }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+  private Node outerNode(Node node) {
+    Node outerNode = centeredNode(node);
+    outerNode.setOnScroll(
+        e -> {
+          e.consume();
+          onScroll(e.getTextDeltaY(), new Point2D(e.getX(), e.getY()));
+          testLabel.setText(
+              "H: "
+                  + scrollPane.getHeight()
+                  + ""
+                  + "W: "
+                  + scrollPane.getWidth()
+                  + "Scale: "
+                  + mapPane.getScaleX());
+        });
+    return outerNode;
+  }
 
-    }
+  private Node centeredNode(Node node) {
+    VBox vBox = new VBox(node);
+    vBox.setAlignment(Pos.CENTER);
+    return vBox;
+  }
+
+  private void updateScale() {
+    mapPane.setScaleX(scaleValue);
+    mapPane.setScaleY(scaleValue);
+  }
+
+  private void onScroll(double wheelDelta, Point2D mousePoint) {
+    double zoomFactor = Math.exp(wheelDelta * zoomIntensity);
+
+    Bounds innerBounds = zoomNode.getLayoutBounds();
+    Bounds viewportBounds = scrollPane.getViewportBounds();
+    System.out.println(
+        innerBounds.getWidth() + " " + viewportBounds.getWidth() + " " + scrollPane.getHvalue());
+
+    // calculate pixel offsets from [0, 1] range
+    double valX = scrollPane.getHvalue() * (innerBounds.getWidth() - viewportBounds.getWidth());
+    double valY = scrollPane.getVvalue() * (innerBounds.getHeight() - viewportBounds.getHeight());
+
+    scaleValue = Math.max(1, scaleValue * zoomFactor);
+    updateScale();
+    scrollPane.layout(); // refresh ScrollPane scroll positions & target bounds
+
+    // convert target coordinates to zoomTarget coordinates
+    Point2D posInZoomTarget = mapPane.parentToLocal(zoomNode.parentToLocal(mousePoint));
+
+    // calculate adjustment of scroll position (pixels)
+    Point2D adjustment =
+        mapPane
+            .getLocalToParentTransform()
+            .deltaTransform(posInZoomTarget.multiply(zoomFactor - 1));
+
+    // convert back to [0, 1] range
+    // (too large/small values are automatically corrected by ScrollPane)
+    Bounds updatedInnerBounds = zoomNode.getBoundsInLocal();
+    scrollPane.setHvalue(
+        (valX + adjustment.getX()) / (updatedInnerBounds.getWidth() - viewportBounds.getWidth()));
+    scrollPane.setVvalue(
+        (valY + adjustment.getY()) / (updatedInnerBounds.getHeight() - viewportBounds.getHeight()));
+  }
 }
-
-
