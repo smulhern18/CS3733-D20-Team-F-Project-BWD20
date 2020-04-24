5c480b7464c93bae942843bc901845ef0b0649d1
denver blake <denver.blake26@gmail.com>
2020-04-24 13:05:39 -0400

    UISetting classes

---
+++ src/main/java/edu/wpi/teamF/Controllers/UISettings/LocationComboBox.java
@@ -0,0 +1,8 @@
+package edu.wpi.teamF.Controllers.UISettings;
+
+import com.jfoenix.controls.JFXComboBox;
+
+public class LocationComboBox {
+    public void setAsLocationComboBox(JFXComboBox comboBox) {
+    }
+}
---
+++ src/main/java/edu/wpi/teamF/Controllers/UISettings/Zoom.java
@@ -0,0 +1,11 @@
+package edu.wpi.teamF.Controllers.UISettings;
+
+import javafx.scene.layout.Pane;
+
+public class Zoom {
+
+    public void makeZoomable(Pane pane) {
+
+
+    }
+}
