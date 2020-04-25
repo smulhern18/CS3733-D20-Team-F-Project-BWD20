54d527ef6b9daffa0b6855903271049c073e960f
rfcarnemolla <rfcarnemolla@wpi.edu>
2020-04-24 21:17:45 -0400

    pathfind null error and no nodes on other maps

--- src/main/java/edu/wpi/teamF/Controllers/PathfinderController.java
+++ src/main/java/edu/wpi/teamF/Controllers/PathfinderController.java
@@ -27,13 +27,27 @@ public class PathfinderController implements Initializable {
 
   public static int MAP_HEIGHT = 1485;
   public static int MAP_WIDTH = 2475;
-  public AnchorPane mapPane;
-  public StackPane masterPane;
+  public AnchorPane currentPane;
+  public AnchorPane mapPaneFaulkner5;
+  public AnchorPane mapPaneFaulkner4;
+  public AnchorPane mapPaneFaulkner3;
+  public AnchorPane mapPaneFaulkner2;
+  public AnchorPane mapPaneFaulkner1;
+  public StackPane masterPaneFaulkner1;
+  public StackPane masterPaneFaulkner2;
+  public StackPane masterPaneFaulkner3;
+  public StackPane masterPaneFaulkner4;
+  public StackPane masterPaneFaulkner5;
   public List<Node> nodeList;
   public List<Node> fullNodeList;
   public JFXButton stairsBtn;
   public JFXButton elevBtn;
   public JFXButton bathBtn;
+  public JFXButton floor1Button;
+  public JFXButton floor2Button;
+  public JFXButton floor3Button;
+  public JFXButton floor4Button;
+  public JFXButton floor5Button;
   public Text commandText;
   public JFXComboBox startCombo;
   public JFXComboBox endCombo;
@@ -54,8 +68,8 @@ public class PathfinderController implements Initializable {
 
     List<Node> nodeList = path.getPath();
 
-    double heightRatio = mapPane.getHeight() / MAP_HEIGHT;
-    double widthRatio = mapPane.getWidth() / MAP_WIDTH;
+    double heightRatio = currentPane.getHeight() / MAP_HEIGHT;
+    double widthRatio = currentPane.getWidth() / MAP_WIDTH;
 
     for (int i = 0; i < nodeList.size() - 1; i++) {
       int startX = (int) (nodeList.get(i).getXCoord() * widthRatio);
@@ -65,14 +79,14 @@ public class PathfinderController implements Initializable {
       Line line = new Line(startX, startY, endX, endY);
       line.setStroke(Color.RED);
       line.setStrokeWidth(2);
-      mapPane.getChildren().add(line);
+      currentPane.getChildren().add(line);
     }
   }
 
   public void placeButton(Node node) {
 
-    double heightRatio = (double) mapPane.getPrefHeight() / MAP_HEIGHT;
-    double widthRatio = (double) mapPane.getPrefWidth() / MAP_WIDTH;
+    double heightRatioFaulkner5 = (double) currentPane.getPrefHeight() / MAP_HEIGHT;
+    double widthRatioFaulkner5 = (double) currentPane.getPrefWidth() / MAP_WIDTH;
 
     JFXButton button = new JFXButton();
     button.setId(node.getId());
@@ -82,12 +96,12 @@ public class PathfinderController implements Initializable {
     button.setStyle(
         "-fx-background-radius: 6px; -fx-border-radius: 6px; -fx-background-color: #3281a8; -fx-border-color: #000000; -fx-border-width: 1px"); // ff0000
 
-    int xPos = (int) ((node.getXCoord() * widthRatio) - 6);
-    int yPos = (int) ((node.getYCoord() * heightRatio) - 6);
+    int xPos = (int) ((node.getXCoord() * widthRatioFaulkner5) - 6);
+    int yPos = (int) ((node.getYCoord() * heightRatioFaulkner5) - 6);
 
     button.setLayoutX(xPos);
     button.setLayoutY(yPos);
-    mapPane.getChildren().add(button);
+    currentPane.getChildren().add(button);
     button.setOnAction(
         actionEvent -> {
           if (startNode == node && state == 1) { // Click again to de-select if start has been set
@@ -127,7 +141,7 @@ public class PathfinderController implements Initializable {
   }
 
   public void resetPane() {
-    mapPane.getChildren().clear();
+    currentPane.getChildren().clear();
     startNode = null;
     endNode = null;
     state = 0;
@@ -150,14 +164,6 @@ public class PathfinderController implements Initializable {
             state = 1;
             commandText.setText("Select End Location or Building Feature");
             endCombo.setDisable(false);
-
-            //            Circle startCircle = new Circle();
-            //            startCircle.setCenterX(startNode.getXCoord());
-            //            startCircle.setCenterY(startNode.getYCoord());
-            //            startCircle.setStyle(
-            //                "-fx-background-radius: 6px; -fx-border-radius: 6px;
-            // -fx-background-color: #ff0000; -fx-border-color: #000000; -fx-border-width: 1px"); //
-            // 800000
           }
         });
 
@@ -168,12 +174,6 @@ public class PathfinderController implements Initializable {
             state = 2;
             commandText.setText("Select Find Path or Reset");
             pathButton.setDisable(false);
-            //            Circle endCircle = new Circle();
-            //            endCircle.setCenterX(endNode.getXCoord());
-            //            endCircle.setCenterY(endNode.getYCoord());
-            //            endCircle.setStyle(
-            //                "-fx-background-radius: 6px; -fx-border-radius: 6px;
-            // -fx-background-color: #00cc00; -fx-border-color: #000000; -fx-border-width: 1px");
           }
         });
 
@@ -190,21 +190,19 @@ public class PathfinderController implements Initializable {
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     nodeList = new ArrayList<>();
-    // fullNodeList = nodeFactory.getAllNodes();
     fullNodeList = new ArrayList<>();
+    currentPane = mapPaneFaulkner5;
+    setAllInvisible();
+    masterPaneFaulkner5.setVisible(true);
+    floorButtonsSet();
 
     for (Node node : nodeFactory.getAllNodes()) {
       fullNodeList.add(node);
       node.setEdges(edgeFactory.getAllEdgesConnectedToNode(node.getId()));
-      //      if (node.getId().charAt(node.getId().length() - 1) == '5') {
-      //        nodeList.add(node);
-      //        node.setEdges(edgeFactory.getAllEdgesConnectedToNode(node.getId()));
-      // System.out.println(node.getId() + " - " + node.getNeighborNodes());
-      //      }
     }
 
     for (Node node : fullNodeList) {
-      if (node.getId().charAt(node.getId().length() - 1) == '5') { // change for floors
+      if (node.getFloor() == 1) { // change for floors
         nodeList.add(node);
       }
       if (!node.getType().equals(Node.NodeType.getEnum("HALL"))) {
@@ -265,8 +263,6 @@ public class PathfinderController implements Initializable {
 
     pathButton.setOnAction(
         actionEvent -> {
-          //          choiceSelectEnd();
-          //          choiceSelectStart();
           Path path = null;
           try {
             path = pathFindAlgorithm.pathfind(startNode, endNode);
@@ -280,5 +276,59 @@ public class PathfinderController implements Initializable {
             e.printStackTrace();
           }
         });
+  }
+
+  public void floorButtonsSet() {
+    floor1Button.setOnAction(
+        actionEvent -> {
+          currentPane = mapPaneFaulkner1;
+          setNodeList(1);
+          setAllInvisible();
+          masterPaneFaulkner1.setVisible(true);
+        });
+    floor2Button.setOnAction(
+        actionEvent -> {
+          currentPane = mapPaneFaulkner1;
+          setNodeList(2);
+          setAllInvisible();
+          masterPaneFaulkner2.setVisible(true);
+        });
+    floor3Button.setOnAction(
+        actionEvent -> {
+          currentPane = mapPaneFaulkner1;
+          setNodeList(3);
+          setAllInvisible();
+          masterPaneFaulkner3.setVisible(true);
+        });
+    floor4Button.setOnAction(
+        actionEvent -> {
+          currentPane = mapPaneFaulkner1;
+          setNodeList(4);
+          setAllInvisible();
+          masterPaneFaulkner4.setVisible(true);
+        });
+    floor5Button.setOnAction(
+        actionEvent -> {
+          currentPane = mapPaneFaulkner1;
+          setNodeList(5);
+          setAllInvisible();
+          masterPaneFaulkner5.setVisible(true);
+        });
+  }
+
+  public void setNodeList(int floorNum) {
+    for (Node node : fullNodeList) {
+      if (node.getFloor() == floorNum) { // change for floors
+        nodeList.add(node);
+      }
+    }
+  }
+
+  public void setAllInvisible() {
+    masterPaneFaulkner1.setVisible(false);
+    masterPaneFaulkner2.setVisible(false);
+    masterPaneFaulkner3.setVisible(false);
+    masterPaneFaulkner4.setVisible(false);
+    masterPaneFaulkner5.setVisible(false);
   }
 }
