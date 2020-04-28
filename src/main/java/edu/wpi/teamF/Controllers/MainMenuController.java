89cabd198ec660f3aa1d0e5d1f883ad7b84c8878
Roman Wicky <rwickyvd@gmail.com>
2020-04-28 19:04:01 -0400

    merging with kevin:

--- src/main/java/edu/wpi/teamF/Controllers/MainMenuController.java
+++ src/main/java/edu/wpi/teamF/Controllers/MainMenuController.java
@@ -85,15 +85,15 @@ public class MainMenuController implements Initializable {
             });
   }
 
-  //  private void resize(double width) {
-  //    System.out.println(width);
-  //    Font newFont = new Font(width / 50);
-  //    loginImage.setFitWidth(gridPane.getCellBounds(0, 1).getWidth());
-  //    loginImage.setFitHeight(gridPane.getCellBounds(0, 1).getHeight());
-  //    pathImage.setFitWidth(gridPane.getCellBounds(0, 2).getWidth());
-  //    pathImage.setFitHeight(gridPane.getCellBounds(0, 2).getHeight());
-  //    helpImage.setFitHeight(gridPane.getCellBounds(0, 3).getWidth());
-  //    helpImage.setFitWidth(gridPane.getCellBounds(0, 3).getHeight());
-  //    welcomeLabel.setFont(newFont);
-  //  }
+  //    private void resize(double width) {
+  //      System.out.println(width);
+  //      Font newFont = new Font(width / 50);
+  //      loginImage.setFitWidth(gridPane.getCellBounds(0, 1).getWidth());
+  //      loginImage.setFitHeight(gridPane.getCellBounds(0, 1).getHeight());
+  //      pathImage.setFitWidth(gridPane.getCellBounds(0, 2).getWidth());
+  //      pathImage.setFitHeight(gridPane.getCellBounds(0, 2).getHeight());
+  //      helpImage.setFitHeight(gridPane.getCellBounds(0, 3).getWidth());
+  //      helpImage.setFitWidth(gridPane.getCellBounds(0, 3).getHeight());
+  //      welcomeLabel.setFont(newFont);
+  //    }
 }
