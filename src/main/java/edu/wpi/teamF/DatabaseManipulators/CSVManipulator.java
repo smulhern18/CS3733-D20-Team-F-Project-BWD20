318f2dbba6ce1a3a07fe57fd010ae76ff17ce47e
tjcampbel <47730819+tjcampbel@users.noreply.github.com>
2020-05-01 21:02:26 -0400

    App upload

--- src/main/java/edu/wpi/teamF/DatabaseManipulators/CSVManipulator.java
+++ src/main/java/edu/wpi/teamF/DatabaseManipulators/CSVManipulator.java
@@ -1170,4 +1170,44 @@ public class CSVManipulator {
             + m.getDateTimeCompleted().getTime();
     return Main;
   }
+
+  public void uploadDatabase(Path path){
+
+    try {
+      InputStream node = new FileInputStream(path.toString() + "/MapFAllnodes.csv");
+      InputStream edge = new FileInputStream(path.toString() + "/MapFAlledges.csv");
+      InputStream account = new FileInputStream(path.toString() + "/AccountBackup.csv");
+      InputStream maintenanceRequest = new FileInputStream(path.toString() + "/MaintenanceBackup.csv");
+      InputStream computerservice = new FileInputStream(path.toString() + "/ComputerBackup.csv");
+      InputStream flowerService= new FileInputStream(path.toString() + "/FlowerBackup.csv");
+      InputStream LanguageService = new FileInputStream(path.toString() + "/LanguageBackup.csv");
+      InputStream LaundryService = new FileInputStream(path.toString() + "/LaundryBackup.csv");
+      InputStream MariachiService= new FileInputStream(path.toString() + "/MariachiBackup.csv");
+      InputStream medicineService = new FileInputStream(path.toString() + "/MedicineBackup.csv");
+      InputStream sanitationRequest = new FileInputStream(path.toString() + "/SanitationBackup.csv");
+      InputStream securityRequest= new FileInputStream(path.toString() + "/SecurityBackup.csv");
+      InputStream transportService= new FileInputStream(path.toString() + "/TransportBackup.csv");
+      readCSVFileNode(node);
+      readCSVFileEdge(edge);
+      readCSVFileAccount(account);
+      readCSVFileMaintenanceService(maintenanceRequest);
+      readCSVFileComputerService(computerservice);
+      readCSVFileFlowerService(flowerService);
+      readCSVFileLanguageService(LanguageService);
+      readCSVFileLaundryService(LaundryService);
+      readCSVFileMariachiService(MariachiService);
+      readCSVFileMedicineDeliveryService(medicineService);
+      readCSVFileSanitationService(sanitationRequest);
+      readCSVFileSecurityService(securityRequest);
+      readCSVFileTransportService(transportService);
+    } catch (Exception e){
+      System.out.println(e.getMessage());
+    }
+
+
+
+
+
+  }
+
 }
