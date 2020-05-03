0dab361a745f951b29205e68b753aab261137eb6
kaboenisch <kaboenisch@wpi.edu>
2020-05-02 22:32:24 -0400

    Added all languages to translator

--- src/main/java/edu/wpi/teamF/Controllers/TranslatorController.java
+++ src/main/java/edu/wpi/teamF/Controllers/TranslatorController.java
@@ -13,6 +13,8 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
@@ -33,11 +35,23 @@ public class TranslatorController implements Initializable {
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     langCodeMap = new HashMap<>();
-    langCodeMap.put("English", "en");
+    langCodeMap.put("Swedish", "sv");
     langCodeMap.put("Spanish", "es");
-    langCodeMap.put("French", "fr");
-    langCodeMap.put("German", "de");
+    langCodeMap.put("Russian", "ru");
+    langCodeMap.put("Portuguese", "pt");
+    langCodeMap.put("Polish", "pl");
+    langCodeMap.put("Norwegian", "no");
+    langCodeMap.put("Korean", "ko");
+    langCodeMap.put("Japanese", "ja");
     langCodeMap.put("Italian", "it");
+    langCodeMap.put("German", "de");
+    langCodeMap.put("French", "fr");
+    langCodeMap.put("Finnish", "fi");
+    langCodeMap.put("English", "en");
+    langCodeMap.put("Dutch", "nl");
+    langCodeMap.put("Danish", "da");
+    langCodeMap.put("Chinese (Mandarin)", "zh");
+    langCodeMap.put("Chinese (Cantonese)", "zh-TW");
 
     langVoicesMap = new HashMap<>();
     langVoicesMap.put("English", Say.Language.EN_US.toString());
@@ -45,11 +59,60 @@ public class TranslatorController implements Initializable {
     langVoicesMap.put("French", Say.Language.FR_CA.toString());
     langVoicesMap.put("German", Say.Language.DE_DE.toString());
     langVoicesMap.put("Italian", Say.Language.IT_IT.toString());
+    langVoicesMap.put("Danish", Say.Language.DA_DK.toString());
+    langVoicesMap.put("Finnish", Say.Language.FI_FI.toString());
+    langVoicesMap.put("Japanese", Say.Language.JA_JP.toString());
+    langVoicesMap.put("Korean", Say.Language.KO_KR.toString());
+    langVoicesMap.put("Norwegian", Say.Language.NB_NO.toString());
+    langVoicesMap.put("Dutch", Say.Language.NL_NL.toString());
+    langVoicesMap.put("Polish", Say.Language.PL_PL.toString());
+    langVoicesMap.put("Portuguese", Say.Language.PT_PT.toString());
+    langVoicesMap.put("Russian", Say.Language.RU_RU.toString());
+    langVoicesMap.put("Swedish", Say.Language.SV_SE.toString());
+    langVoicesMap.put("Chinese (Mandarin)", Say.Language.ZH_CN.toString());
+    langVoicesMap.put("Chinese (Cantonese)", Say.Language.ZH_HK.toString());
 
     fromLanguage.getItems().addAll(langCodeMap.keySet());
     toLanguage.getItems().addAll(langCodeMap.keySet());
 
     resetTranslator();
+
+    callPhone.setDisable(true);
+    sendSMS.setDisable(true);
+    phoneNumber
+        .textProperty()
+        .addListener(
+            new ChangeListener<String>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue.length() == 10
+                    && newValue.matches("[0-9]+")
+                    && textOutput.getText().length() > 0) {
+                  callPhone.setDisable(false);
+                  sendSMS.setDisable(false);
+                } else {
+                  callPhone.setDisable(true);
+                  sendSMS.setDisable(true);
+                }
+              }
+            });
+
+    translate.setDisable(true);
+    textInput
+        .textProperty()
+        .addListener(
+            new ChangeListener<String>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue.length() > 0) {
+                  translate.setDisable(false);
+                } else {
+                  translate.setDisable(true);
+                }
+              }
+            });
 
     // TODO Disable translate button if text is empty
   }
@@ -86,6 +149,11 @@ public class TranslatorController implements Initializable {
   }
 
   public void translate(ActionEvent actionEvent) throws IOException {
+    if (phoneNumber.getText().length() == 10 && phoneNumber.getText().matches("[0-9]+")) {
+      sendSMS.setDisable(false);
+      callPhone.setDisable(false);
+    }
+
     textOutput.setText(
         translateTextWithModel(
             langCodeMap.get(fromLanguage.getValue()),
@@ -119,5 +187,17 @@ public class TranslatorController implements Initializable {
 
     System.out.println(callText);
     phoneComms.callPhone(phoneNumber.getText(), callText);
+  }
+
+  public void toLanguage(ActionEvent actionEvent) {
+    textOutput.setText("");
+    sendSMS.setDisable(true);
+    callPhone.setDisable(true);
+  }
+
+  public void fromLanguage(ActionEvent actionEvent) {
+    textOutput.setText("");
+    sendSMS.setDisable(true);
+    callPhone.setDisable(true);
   }
 }
