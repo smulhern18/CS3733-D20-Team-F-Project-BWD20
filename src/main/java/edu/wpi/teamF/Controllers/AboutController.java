2c1646f4d99d6d11e5f2e1be98ee7e31e17eed91
kaboenisch <kaboenisch@wpi.edu>
2020-05-06 01:06:39 -0400

    About page FXML

--- src/main/java/edu/wpi/teamF/Controllers/AboutController.java
+++ src/main/java/edu/wpi/teamF/Controllers/AboutController.java
@@ -1,3 +1,20 @@
 package edu.wpi.teamF.Controllers;
 
-public class AboutController {}
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class AboutController implements Initializable {
+  public AnchorPane frame;
+  public ImageView backgroundImage;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    //    resize(anchorPane.getWidth());
+    backgroundImage.setPreserveRatio(false);
+    backgroundImage.fitHeightProperty().bind(frame.heightProperty());
+    backgroundImage.fitWidthProperty().bind(frame.widthProperty());
+  }
+}
