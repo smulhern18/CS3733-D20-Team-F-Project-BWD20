de245196a8f4a84413461325f52299c30e22f0fb
Reilly Norum <renorum@wpi.edu>
2020-05-10 17:07:24 -0400

    social media linked

--- src/main/java/edu/wpi/teamF/Controllers/ContactUsController.java
+++ src/main/java/edu/wpi/teamF/Controllers/ContactUsController.java
@@ -1,31 +1,57 @@
 package edu.wpi.teamF.Controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 
-public class ContactUsController {
+public class ContactUsController implements Initializable {
 
   public WebView webView;
   public AnchorPane web;
-  //  SceneController sceneController = App.getSceneController();
   public ImageView facebook;
+  public ImageView linked;
+  public ImageView instagram;
+  public ImageView youtube;
+  public ImageView twitter;
+  public ImageView blog;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
 
   @FXML
   private void toFacebook(MouseEvent mouseEvent) throws IOException {
     webView.getEngine().load("https://www.facebook.com/BrighamandWomensHospital/");
     web.setVisible(true);
+  }
 
-    // sceneController.switchScene("webView");
-    //    primaryStage.setTitle("what");
-    //    WebView w = new WebView();
-    //    final WebEngine e = w.getEngine();
-    //    e.load("https://www.facebook.com/BrighamandWomensHospital/");
-    //    Scene scene = new Scene(w, w.getPrefWidth(), w.getPrefHeight());
-    //    w.switchScene(scene);
-    //    primaryStage.show();
+  public void toLinkedIn(MouseEvent mouseEvent) throws IOException {
+    webView.getEngine().load("https://www.linkedin.com/company/brigham-and-women%27s-hospital");
+    web.setVisible(true);
+  }
+
+  public void toInstagram(MouseEvent mouseEvent) {
+    webView.getEngine().load("https://www.instagram.com/brighamandwomens/?hl=en");
+    web.setVisible(true);
+  }
+
+  public void toYoutube(MouseEvent mouseEvent) {
+    webView.getEngine().load("https://www.youtube.com/user/Brighamandwomens");
+    web.setVisible(true);
+  }
+
+  public void toTwitter(MouseEvent mouseEvent) {
+    webView.getEngine().load("https://twitter.com/BrighamWomens");
+    web.setVisible(true);
+  }
+
+  public void toBlog(MouseEvent mouseEvent) {
+    webView.getEngine().load("https://brighamhealthhub.org/");
+    web.setVisible(true);
   }
 }
